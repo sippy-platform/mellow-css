3e21cddf2d013534c6b9dedb8932041899515ec9
Yannick <yannick.franssen@outlook.com>
2022-02-18 16:26:48 +0100

    Start implementing tabs, import Bootstrap JS

--- css/js/index.js
+++ css/js/index.js
@@ -1,1 +1,2 @@
 export { default as OffCanvas } from './src/offCanvas';
+export { default as Tab } from './src/tab';
---
+++ css/js/src/util/component-functions.js
@@ -0,0 +1,31 @@
+/**
+ * Based on Boostrap v5.1.3 - tab.js - MIT
+ */
+
+import EventHandler from '../dom/event-handler';
+import { getElementFromSelector, isDisabled } from './index';
+
+const enableDismissTrigger = (component, method = 'hide') => {
+  const clickEvent = `click.dismiss${component.EVENT_KEY}`;
+  const name = component.NAME;
+
+  EventHandler.on(document, clickEvent, `[data-mellow-dismiss="${name}"]`, function (event) {
+    if (['A', 'AREA'].includes(this.tagName)) {
+      event.preventDefault();
+    }
+
+    if (isDisabled(this)) {
+      return;
+    }
+
+    const target = getElementFromSelector(this) || this.closest(`.${name}`);
+    const instance = component.getOrCreateInstance(target);
+
+    // Method argument is left, for Alert and only, as it doesn't implement the 'hide' method
+    instance[method]();
+  });
+};
+
+export {
+  enableDismissTrigger
+};
