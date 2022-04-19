d0ca3a398c4c2fd20b9135827b9a90e031db7b9c
Yannick <yannick.franssen@outlook.com>
2022-04-19 15:50:33 +0200

    Add labels, implement for checkboxes

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,5 +1,7 @@
 export { default as Button } from "./Button";
-export { default as Grid } from "./Grid";
+export { default as Checkbox } from "./Checkbox";
 export { default as Col } from "./Col";
 export { default as Container } from "./Container";
+export { default as Grid } from "./Grid";
+export { default as Label } from "./Label";
 export { default as Progress } from "./Progress";
