ccff18e913e71c51afcceebb9015207f51209f77
Yannick <yannick.franssen@outlook.com>
2022-04-19 14:04:52 +0200

    Implement Checkbox

---
+++ ui/src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+import * as CheckboxPrimitives from '@radix-ui/react-checkbox';
+
+import "../mellow.css";
+
+import clsx from "clsx";
+
+export interface CheckboxProps {
+  /**
+   * Whether or not the checkbox is checked
+   */
+  checked?: boolean;
+  /**
+   * Custom classes for the checkbox
+   */
+  className?: string;
+  /**
+   * Children
+   */
+  children?: ReactNode;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Checkbox = ({
+  className
+}: CheckboxProps) => {
+  return (
+    <div className={clsx('input-form', className)}>
+      <CheckboxPrimitives.Root className="input-check" defaultChecked id="firstCheckbox" />
+      <label className="input-label" htmlFor="firstCheckbox">
+        First checkbox
+      </label>
+    </div>
+  );
+};
+
+export default Checkbox;
