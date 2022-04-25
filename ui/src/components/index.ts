901bfc37fdaffb8dfa78d2e2319c13424015d381
Yannick <yannick.franssen@outlook.com>
2022-04-25 16:48:32 +0200

    Implement Alert

--- ui/src/components/index.ts
+++ ui/src/components/index.ts
@@ -1,3 +1,5 @@
+export { default as Alert } from './Alert';
+export { default as AlertTitle } from './AlertTitle';
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
 export { default as Col } from './Col';
