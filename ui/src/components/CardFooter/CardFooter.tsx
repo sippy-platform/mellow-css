89366c8f2b1953d5977f6686dd19b64a0623400f
Yannick <yannick.franssen@outlook.com>
2022-04-25 16:16:35 +0200

    Add Footer, Header, Link

---
+++ ui/src/components/CardFooter/CardFooter.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react';
+import '@sippy/mellow-css/dist/css/mellow.css';
+
+import clsx from 'clsx';
+
+export interface CardFooterProps {
+  /**
+   * Custom classes for the dialog
+   */
+  className?: string;
+  /**
+   * CardFooter contents.
+   */
+  children: ReactNode;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function CardFooter({
+  className,
+  children,
+  ...props
+}: CardFooterProps) {
+  return (
+    <div
+      className={clsx(
+        'card-footer',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default CardFooter;
