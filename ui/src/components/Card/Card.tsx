c50839646e3575d34fe3f6befa19430937348449
Yannick <yannick.franssen@outlook.com>
2022-04-25 15:03:06 +0200

    Start implementing Cards

---
+++ ui/src/components/Card/Card.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react';
+import '@sippy/mellow-css/dist/css/mellow.css';
+
+import clsx from 'clsx';
+
+export interface CardProps {
+  /**
+   * Custom classes for the dialog
+   */
+  className?: string;
+  /**
+   * Card contents.
+   */
+  children: ReactNode;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function Card({
+  className,
+  children,
+  ...props
+}: CardProps) {
+  return (
+    <div
+      className={clsx(
+        'card',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
---
+++ ui/src/components/CardTitle/CardTitle.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react';
+import '@sippy/mellow-css/dist/css/mellow.css';
+
+import clsx from 'clsx';
+
+export interface CardTitleProps {
+  /**
+   * Custom classes for the dialog
+   */
+  className?: string;
+  /**
+   * CardTitle contents.
+   */
+  children: ReactNode;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export function CardTitle({
+  className,
+  children,
+  ...props
+}: CardTitleProps) {
+  return (
+    <h5
+      className={clsx(
+        'card-title',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </h5>
+  );
+};
+
+export default CardTitle;
