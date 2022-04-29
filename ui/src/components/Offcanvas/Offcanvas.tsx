92bd1b99d941d2e3fbf78f5a144b70a425bb2dfb
Yannick <yannick.franssen@outlook.com>
2022-04-29 11:50:59 +0200

    Start implementing Offcanvas with HUI

--- ui/src/components/Offcanvas/Offcanvas.tsx
+++ ui/src/components/Offcanvas/Offcanvas.tsx
@@ -1,8 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { Fragment, ReactNode, useState } from 'react';
 
 import * as DialogPrimitives from '@radix-ui/react-dialog';
+import { Dialog, Transition } from '@headlessui/react'
 
 import clsx from 'clsx';
+import Button from '../Button/Button';
+import ValkyrieIcon, { viXmark } from '@optimisegroup/valkyrie';
 
 export interface OffcanvasProps {
   /**
@@ -38,27 +41,56 @@ export const Offcanvas = ({
   position = 'start',
   ...props
 }: OffcanvasProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <DialogPrimitives.Root
-      modal={!disableModal}
-      {...props}
-    >
-      <DialogPrimitives.Trigger asChild>
-        {trigger}
-      </DialogPrimitives.Trigger>
-      <DialogPrimitives.Portal>
-        <DialogPrimitives.Overlay className="backdrop" />
-        <DialogPrimitives.Content
-          className={clsx(
-            'offcanvas offcanvas-animate',
-            `offcanvas-${position}`,
-            className
-          )}
-        >
-          {children}
-        </DialogPrimitives.Content>
-      </DialogPrimitives.Portal>
-    </DialogPrimitives.Root>
+    <>
+      <Button onClick={() => setOpen(true)}>Open canvas</Button>
+      <Transition appear show={open} as={Fragment}>
+        <Dialog onClose={() => setOpen(false)} className="position-relative">
+          <Transition.Child
+            as={Fragment}
+            enter="transition-all duration-200 ease-inout"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-all duration-200 ease-inout"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="backdrop" aria-hidden="true" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition-all duration-200 ease-inout"
+            enterFrom="transform-x-n100"
+            enterTo="transform-none"
+            leave="transition-all duration-200 ease-inout"
+            leaveFrom="transform-none"
+            leaveTo="transform-x-n100"
+          >
+            <Dialog.Panel className="offcanvas offcanvas-start">
+              <div className="offcanvas-header">
+                <Dialog.Title className="offcanvas-title" as="h5">Deactivate account</Dialog.Title>
+                <button value="cancel" className="btn-close" onClick={() => setOpen(false)}><ValkyrieIcon icon={viXmark} /></button>
+              </div>
+              <div className="offcanvas-body">
+                <Dialog.Description>
+                  This will permanently deactivate your account
+                </Dialog.Description>
+
+                <p>
+                  Are you sure you want to deactivate your account? All of your data
+                  will be permanently removed. This action cannot be undone.
+                </p>
+
+                <Button onClick={() => setOpen(false)}>Deactivate</Button>
+                <Button onClick={() => setOpen(false)}>Cancel</Button>
+              </div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
 
